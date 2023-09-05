40e50e816efc487dea7376a97f479842aaf7a058
Kenz0-61 <enes.eou@outlook.com>
2023-09-05 19:11:54 +0300

    DataAccessLayer

---
+++ Project.Dal/Context/MyContext.cs
@@ -0,0 +1,38 @@
+﻿using Project.Entites.Models;
+using Project.Map.Options;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Dll.Context
+{
+    public class MyContext : DbContext
+    {
+        public MyContext() : base("MyConnection")
+        {
+
+        }
+        //FluentApi Tablo Konfigrasyonlarının yazılıdı onModelCreating methodu Overide ediyoruz.Mapleri ekliyoruz.
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new AppUserMap());
+            modelBuilder.Configurations.Add(new OrderMap());
+            modelBuilder.Configurations.Add(new AppUserProfileMap());
+            modelBuilder.Configurations.Add(new OrderDetailMap());
+            modelBuilder.Configurations.Add(new ShipperMap());
+            modelBuilder.Configurations.Add(new ProductMap());
+            modelBuilder.Configurations.Add(new CategoryMap());
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<AppUser> AppUsers { get; set; }
+        public DbSet<Category> Categorys { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<Shipper> Shippers { get; set; }
+        public DbSet<AppUserProfile> AppUserProfiles { get; set; }
+    }
+}
